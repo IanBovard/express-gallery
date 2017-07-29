8785e90e3e07e1e0ba24280c1eafed7f20867d7e
Ian Bovard <thebestpantsintown@gmail.com>
2017-07-28 14:01:57 -1000

    no scss

---
+++ server.js
@@ -0,0 +1,18 @@
+/* jshint esversion:6*/
+const express = require('express');
+const exphbs = require ('express-handlebars');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 8080;
+
+let db = require('./models');
+let Users = db.Users;
+
+const app = express();
+const hbs = exphbs.create({
+  defaultLayout :'main',
+  extname : 'hbs'
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
