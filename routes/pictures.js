b91d9ea4c063d5fb1d72433f7ba8fc9ff98712d3
Kristin <kmtreder@hawaii.edu>
2017-07-31 19:08:02 -1000

    add get all, get by id, post, and put by id

--- routes/pictures.js
+++ routes/pictures.js
@@ -5,12 +5,43 @@ const router = express.Router();
 let db = require('../models');
 let Pictures = db.Pictures;
 
-router.get('/', (req, res) =>{
-  return Pictures.findAll()
+router.get('/', (req, res) => {
+  return Pictures.findAll({raw:true})
   .then(pictures => {
+    console.log({pictures: pictures});
     res.render('pictures/index', { pictures: pictures });
   });
 });
 
+router.get('/:id', (req, res) => {
+  let id = req.params.id;
+  return Pictures.findById(id)
+  .then(picture => {
+    console.log(picture.dataValues);
+    //res.render('pictures/picture', { picture: picture });
+  })
+})
+
+router.post('/', (req, res) => {
+  return Pictures.create({ user_id: 1, author: req.body.author, link: req.body.link, description: req.body.description })
+  .then(picture => {
+    //console.log(picture);
+    res.redirect('pictures/index')
+  });
+});
+
+router.post('/:id', (req, res) => {
+  let id = req.params.id;
+  return Pictures.findById(id)
+  .then(picture => {
+    //console.log(picture);
+    picture.update( {author: req.body.author, link: req.body.link, description: req.body.description })
+    return picture;
+  })
+  .then(picture => {
+    res.redirect('pictures/index')
+  })
+
+});
 
 module.exports = router;
