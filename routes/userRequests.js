43eaa3148e8336fc3c81b6ec8eacf7f4c22468df
Ian Bovard <thebestpantsintown@gmail.com>
2017-08-04 20:03:16 -1000

    username path WIP

--- routes/userRequests.js
+++ routes/userRequests.js
@@ -12,8 +12,30 @@ let Gallery = db.Galleries;
 
 //user routes -- /users/:username -- /user/:username/gallery -- /user/:username/galery/:pictureId -- /users/:username//gallery/:pictureId -- /users/:username/gallery/new
 
-router.get('/userlist', (req, res) => {
-  res.render('users/userlist');
+
+router.get('/:username', (req, res) => {
+  let path = req.params.username;
+  return Users.findOne( { where: { username: path }}, { raw: true }).then(user => {
+    if (!req.user.username) { res.render('gallery/index', { user: user});}
+    if (req.user.username !== path) { res.render('gallery/index', { user: user });}
+    if (req.user.username === path) { res.render('gallery/index', { user: user });}
+  });
 });
 
+
+
+/*router.get('/:username', (req, res) => {
+  if (!req.user) { res.render('gallery/index'); }
+  if (req.user) {
+    let username = req.user.username;
+    let path = req.params.username;
+    if (username !== path) { res.render('gallery/index'); }
+    if (username === path) {
+      return Users.findOne( { where: { username: username }}, { raw: true }).then(user => {
+        res.render('gallery/index', { user: user});
+      });
+    }
+  }
+});
+*/
 module.exports = router;
